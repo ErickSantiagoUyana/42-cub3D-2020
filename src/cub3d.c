1b785e172b48f24ad6f3e975f436b9a4897a3eec
Erick Uyana <erick@Mac-de-Erick-2.local>
2020-12-12 14:44:54 +0100

    File read

--- src/cub3d.c
+++ src/cub3d.c
@@ -6,7 +6,7 @@
 /*   By: euyana-b <euyana-b@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/09 13:36:30 by klever            #+#    #+#             */
-/*   Updated: 2020/12/02 23:42:40 by euyana-b         ###   ########.fr       */
+/*   Updated: 2020/12/11 21:55:46 by euyana-b         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,7 +22,9 @@ typedef struct s_file
 	int R[2];
 	int F[3];
 	int C[3];
-	int map;
+	int map_y;
+	int map_x;
+	int date;
 	char **tab;
 }File;
 
@@ -32,14 +34,14 @@ int		t_resolution(File *file, char * line)
 	while (*line == ' ')
 		line++;
 	file->R[0] = ft_atoi(line);
-	
+
 	while (*line != ' ')
 		line++;
 	file->R[1] = ft_atoi(line);
 
 	if (file->R[0] != 0 && file->R[1] != 0)
 		return (1);
-	
+
 	return (0);
 }
 
@@ -72,91 +74,144 @@ int	t_floor_ceiling(File *file, char * line, char type)
 	i = 0;
 	while (*line != '\0')
 	{
-		if (ft_isdigit(*line))
-		{	
+		if (ft_isdigit(*line) )
+		{
+
 			if (type == 'F')
 				file->F[i] = ft_atoi(line);
 			else if (type == 'C')
 					file->C[i] = ft_atoi(line);
+			if (i == 3)
+				return (1);
 			while (*line != ',')
 				line++;
 			i++;
 		}
+
 		line++;
 	}
-	return (1);
+	return (0);
 }
 
 void type (File *file, char * line)
 {
-
+	int aux;
+	aux = 0;
 	while (*line == ' ')
 		line++;
-	
-	if (line[0] == 'R')
-		file->map += t_resolution(file, line);
-	else if (line[0] == 'N' && line[1] == 'O')
-			file->map += t_sprites(file, line,'N');
-	else if (line[0] == 'S' && line[1] == 'O')
-			file->map += t_sprites(file, line,'S');
-	else if (line[0] == 'W' && line[1] == 'E')
-			file->map += t_sprites(file, line,'W');
-	else if (line[0] == 'E' && line[1] == 'A')
-			file->map += t_sprites(file, line,'E');
-	else if (line[0] == 'S')
-			file->map += t_sprites(file, line,'s');
-	else if (line[0] == 'F')
-			file->map += t_floor_ceiling(file, line,'F');
-	else if (line[0] == 'C')
-			file->map += t_floor_ceiling(file, line,'C');
-	
-}
 
+	if (line[0] == 'R')
+		aux = t_resolution(file, line);
+	else if (line[0] == 'N' && line[1] == 'O')
+			aux = t_sprites(file, line,'N');
+	else if (line[0] == 'S' && line[1] == 'O')
+			aux = t_sprites(file, line,'S');
+	else if (line[0] == 'W' && line[1] == 'E')
+			aux = t_sprites(file, line,'W');
+	else if (line[0] == 'E' && line[1] == 'A')
+			aux = t_sprites(file, line,'E');
+	else if (line[0] == 'S')
+			aux= t_sprites(file, line,'s');
+	else if (line[0] == 'F')
+			aux = t_floor_ceiling(file, line,'F');
+	else if (line[0] == 'C')
+			aux = t_floor_ceiling(file, line,'C');
+	file->date += aux;
+}
+int read_map(File *file, char * line)
+{
+	char	**tmp;
+	int		j;
+
+	j = 0;
+	write(1,"L",1);
+	if (!(tmp = malloc(sizeof(char *) * (ft_strlen(line) + 1))))
+		return (0);
+	while (*line != '\0')
+	{
+		tmp[j] = &line[j];
+		j++;
+	}
+	file->tab[file->map_y] = *tmp;
+	free(tmp);
+	file->tab[file->map_y + 1] = NULL;
+	file->map_y++;
+	printf("\n%s",file->tab[file->map_y]);
+	return (1);
+
+}
 int read_file_cub(File *file, char *f_cub)
 {
 	int fd;
 	char *line;
-	file->map = 0;
-	fd = open(f_cub, O_RDONLY);
-	while (get_next_line(fd,&line) != 0 || file->map != 8)
-	{	
-		if(ft_strlen(line) > 0)
-			type(file, line);
-		
-	}
 
-	file->tab[0][0] = "asdasd";
-	printf("%s", file->tab[0][0]);
+	if ((fd = open(f_cub, O_RDONLY)) > 0)
+	{
+		//while (get_next_line(fd,&line) != 0)
+		//{
+
+
+		//	if(ft_strlen(line) > 0)
+		//	{
+		//		type(file, line);
+		//		if (file->date == 8)
+		//			break;
+		//	}
+		//}
+
+		while (get_next_line(fd,&line) != 0)
+		{
+
+			if(ft_strlen(line) > 0)
+			{
+
+				read_map(file, line);
+				printf("\n%s", line);
+			}
+			if (file->date == 8)
+					break;
+		}
+	}
+	else
+		fd = -1;
 	close(fd);
-	return (1);
+	return (fd);
 }
 
 void print_file(File *file)
 {
-	printf("\n%i", file->R[0]);
-	printf("\n%i", file->R[1]);
-	printf("\n%i", file->F[0]);
-	printf("\n%i", file->F[1]);
-	printf("\n%i", file->F[2]);
-	printf("\n%i", file->C[0]);
-	printf("\n%i", file->C[1]);
-	printf("\n%i", file->C[2]);
-	printf("\n%s", file->NO);
-	printf("\n%s", file->SO);
-	printf("\n%s", file->WE);
-	printf("\n%s", file->EA);
-	printf("\n%s", file->S);
+	printf("\nR %i", file->R[0]);
+	printf("\nR %i", file->R[1]);
+	printf("\nF %i", file->F[0]);
+	printf("\nF %i", file->F[1]);
+	printf("\nF %i", file->F[2]);
+	printf("\nC %i", file->C[0]);
+	printf("\nC %i", file->C[1]);
+	printf("\nC %i", file->C[2]);
+	printf("\nNO %s", file->NO);
+	printf("\nSO %s", file->SO);
+	printf("\nWE %s", file->WE);
+	printf("\nEA %s", file->EA);
+	printf("\nS %s", file->S);
 }
+
 
 int		main(int ac, char **av)
 {
 	File file;
-	
-	if (read_file_cub(&file, av[1]))
+	int aux;
+
+
+	aux = 0;
+	if (( aux = read_file_cub(&file, av[1])) > 0)
+	{
 		print_file(&file);
+		printf("\nValor = %i", aux);
+
+	}
 	else
 		printf("Error %i", ac);
-	
+
 
 	return (0);
 }
